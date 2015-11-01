197101b137e692611c11f3d0591bd0937733a7c6
wshh08 <wshh08@126.com>
2015-11-01 16:46:09 +0800

    Init repository.

---
+++ android/src/com/wshh08/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.wshh08.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.wshh08.game.Can;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Can(), config);
+	}
+}
